cc006a8cafdb03db66398e05bb4655d7a4b7e188
SlightlyUnorthodox <gerts.dax1@gmail.com>
2017-02-14 14:40:37 -0500

    Added Simple StateBasedDialogue system with a very brief test case. Ignoring .idea files

--- lib/StateBasedDialogue.py
+++ lib/StateBasedDialogue.py
@@ -3,10 +3,65 @@
 from __future__ import print_function
 
 import re
-import random
-from nltk import compat
+import time
 
-class Chat():
-    #def __
-    def __init__(self):
-        print("This exists")
+
+# import random
+# from nltk import compat
+
+
+class Simple:
+    def __init__(self, system_name, states, patterns):
+        self.system_name = system_name
+        self.states = states
+        self.patterns = patterns
+
+        self.current_state = 'start'
+        self.system_utterance = states['start']
+
+        self.quit = False
+        self.match_found = True
+
+        self.error_message = ''
+        self.user_utterance = ''
+
+    def start(self):
+        while not self.quit:
+            self.generate_system_utterance()
+            self.wait_for_user_utterance()
+            time.sleep(0.5)
+            self.check_state()
+
+    def check_state(self):
+        for state_rule in self.patterns:
+            if self.states_match(state_rule[0]):
+                if self.patterns_match(state_rule):
+                    self.current_state = state_rule[3]
+                    return
+                else:
+                    self.error_message = state_rule[2]
+
+    def states_match(self, pattern_state):
+        return self.current_state == pattern_state
+
+    def patterns_match(self, pattern_tupple):
+        self.match_found = re.match(pattern_tupple[1], self.user_utterance)
+        return self.match_found
+
+    def generate_system_utterance(self):
+        self.check_match_found()
+        print(self.system_name + ": " + self.system_utterance + '\n')
+
+    def check_match_found(self):
+        if self.match_found:
+            self.system_utterance = self.states[self.current_state]
+        else:
+            self.system_utterance = self.error_message
+
+    def wait_for_user_utterance(self):
+        self.user_utterance = str(raw_input("User: "))
+        self.check_for_quit()
+
+    def check_for_quit(self):
+        if self.user_utterance.find('quit') >= 0:
+            self.quit = True
--- lib/test.py
+++ lib/test.py
@@ -3,12 +3,28 @@
 from __future__ import print_function
 from nltk.chat.util import reflections
 import StateBasedDialogue
+import re
 
-states = ()
+# Make sure to include a 'start' and 'end' state
+any_pattern = re.compile(".?")
+
+states = {
+    'start': 'Hello!',
+    'middle': 'How are you?',
+    'next': 'Why should I care?',
+    'end': 'That\'s nice. Goodbye!',
+}
+
+patterns = (
+    ('start', any_pattern, 'Enter any value', 'middle'),
+    ('middle', any_pattern, 'Enter any value', 'next'),
+    ('next', any_pattern, 'Enter any value', 'end'),
+    ('end', any_pattern, 'Enter any value', 'end'),
+)
 
 def demo():
-    test_chatbot = StateBasedDialogue(states)
-    test_chatbot.start_dialogue()
+    test_chatbot = StateBasedDialogue.Simple('Test System', states, patterns)
+    test_chatbot.start()
 
 if __name__ == "__main__":
     demo()
